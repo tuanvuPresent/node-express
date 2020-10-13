3c19c0dc528478a90f6175d638ebae13486e1afe
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2020-10-13 22:02:34 +0700

    Update router.js

--- api/todos/router.js
+++ api/todos/router.js
@@ -1,13 +1,19 @@
 'use strict';
 const todoCtr = require('./controler');
-const isAuth = require('../auth/auth').isAuth
+// const isAuth = require('../auth/auth').isAuth
 const express = require('express')
 const router = express.Router()
 
-router.post('/todos', isAuth, todoCtr.create)
-router.get('/todos', isAuth, todoCtr.get)
-router.get('/todos/:id', isAuth, todoCtr.detail)
-router.put('/todos/:id', isAuth, todoCtr.update)
-router.delete('/todos/:id', isAuth, todoCtr.destroy)
+// router.post('/todos', isAuth, todoCtr.create)
+// router.get('/todos', isAuth, todoCtr.get)
+// router.get('/todos/:id', isAuth, todoCtr.detail)
+// router.put('/todos/:id', isAuth, todoCtr.update)
+// router.delete('/todos/:id', isAuth, todoCtr.destroy)
+
+router.post('/todos', todoCtr.create)
+router.get('/todos', todoCtr.get)
+router.get('/todos/:id', todoCtr.detail)
+router.put('/todos/:id', todoCtr.update)
+router.delete('/todos/:id', todoCtr.destroy)
 
 module.exports = router
