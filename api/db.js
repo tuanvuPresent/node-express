b562a813979ebcb7179fe30422e3398c15373e8a
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2020-10-13 22:08:10 +0700

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,17 +3,22 @@
 const mysql = require('mysql');
 
 //local mysql db connection
-const db = mysql.createConnection({
-    host: process.env.MY_SQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-});
+// const db = mysql.createConnection({
+//     host: process.env.MY_SQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+// });
 // connect to database
-db.connect(err => {
-    if (err) {
-        console.log(err);
-    }
+// db.connect(err => {
+//     if (err) {
+//         console.log(err);
+//     }
+// })
+// posgres
+const { Pool, Client } = require('pg')
+const connectionString = process.env.DATABASE_URL
+const db = new Pool({
+  connectionString: connectionString,
 })
-
 module.exports = db;
