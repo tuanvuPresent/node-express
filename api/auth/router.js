bd5b2663f2ca7fab2936916db9d390127561dcab
VuNT <you@example.com>
2020-12-28 16:20:26 +0700

    first

---
+++ api/auth/router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const authCtrl = require('./controler');
+const express = require('express')
+const router = express.Router()
+
+router.post('/auth/login', authCtrl.login)
+router.post('/auth/register', authCtrl.register)
+
+module.exports = router
---
+++ api/todos/controler.js
@@ -0,0 +1,52 @@
+'use strict'
+const model = require('../../models');
+const Todos = model.todos
+
+module.exports = {
+    create: async (req, res) => {
+        let data = req.body;
+        let todo = await Todos.create({
+            title: data.title
+        })
+        res.json(todo)
+    },
+
+    get: async (req, res) => {
+        let todo = await Todos.findAll()
+        res.json(todo)
+    },
+
+    detail: async (req, res) => {
+        let id = req.params.id
+        let todo = await Todos.findAll({
+            where: {
+                id: id
+            }
+        });
+        res.json(todo[0])
+    },
+
+    update: async (req, res) => {
+        let id = req.params.id
+        let title = req.body.title
+        Todos.update({
+            title: title
+        }, {
+            where: {
+                id: id
+            }
+        });
+        res.json({'message': 'success'})
+    },
+
+    destroy: async (req, res) => {
+        let id = req.params.id
+        Todos.destroy({
+            where: {
+                id: id
+            }
+        });
+        res.json({'message': 'success'})
+    },
+
+}
